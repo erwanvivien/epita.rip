427519c9535e3ce7505fd0e1a2084eb658a6deaa
erwan.vivien <erwan.vivien@epita.fr>
2021-11-29 20:46:25 +0100

    fix: filters only epitean subdomains

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,36 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+import { GetServerSideProps } from 'next'
 
 const subdomainReg =
-  /([A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)\.(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.|localhost)/;
+  /([A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)\.(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\.|localhost:*)/;
 
 const availableDomains = {
-  "gistre": "Gistred",
-  "mti": "MTIed",
-  "scia": "SCIAed",
-  "tcom": "TCOMed",
-  "gitm": "GITM'd",
-  "image": "IMAGEd",
-  "sante": "SANTEd",
-  "sigl": "SIGL'd",
-  "srs": "SRSed",
-  "ice": "Ice'd",
+  gistre: "Gistred",
+  mti: "MTIed",
+  scia: "SCIAed",
+  tcom: "TCOMed",
+  gitm: "GITM'd",
+  image: "IMAGEd",
+  sante: "SANTEd",
+  sigl: "SIGL'd",
+  srs: "SRSed",
+  ice: "Ice'd",
 }
 
-export default function Home({ host }: { host: string }) {
-  const match = host.match(subdomainReg);
-  if (match && match.at(1) in availableDomains) {
+type Props = {
+  subdomain: string,
+  epiteanSub: boolean,
+};
+
+export default function Home({ subdomain, epiteanSub }: Props) {
+  if (epiteanSub) {
+    const text = availableDomains[subdomain];
     return (
       <div className={styles.container}>
-        {availableDomains[match.at(1)]}
+        <h1 className={styles.majeure}>
+          {text}
+        </h1>
       </div>
     )
   }
@@ -36,11 +43,26 @@ export default function Home({ host }: { host: string }) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const host = ctx.req.headers['x-forwarded-host'] || ctx.req.headers['host']
+  const host = ctx.req.headers['x-forwarded-host'] || ctx.req.headers['host'];
+  const subdomainMatch = typeof host === "string"
+    ? host.match(subdomainReg) : host[0].match(subdomainReg);
+  const subdomain = subdomainMatch && subdomainMatch.at(1);
+
+  const epiteanSub = (subdomain in availableDomains);
+
+  if (subdomain && !epiteanSub) {
+    return {
+      redirect: {
+        destination: 'http://localhost:3000',
+        permanent: false,
+      },
+    }
+  }
 
   return {
     props: {
-      host
+      subdomain: subdomain,
+      epiteanSub: epiteanSub,
     }
   }
 }
